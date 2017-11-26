67bca0672ce0f0332e9baef1dcb049b6518838d0
Granyy <leogranier@gmail.com>
2017-11-26 11:23:04 +0100

    [DEV] Add read/write functionnalities

--- dev_esp/ble_demo/main/main.cpp
+++ dev_esp/ble_demo/main/main.cpp
@@ -1,62 +1,87 @@
 /**
- * Create a BLE Server that when it receive a read request from a BLE client for the value
- * of a characteristic will have the BLECharacteristicCallback invoked in its onRead() method.
- * This can be then used to set the value of the corresponding characteristic which will then
- * be returned back to the client.
+ * Create a new BLE server with write/read characteristic.
  */
 
-#include "BLEUtils.h"
+#include "BLEDevice.h"
 #include "BLEServer.h"
+#include "BLEUtils.h"
 #include <esp_log.h>
 #include <string>
+#include <Task.h>
 #include <sys/time.h>
 #include <sstream>
-#include "BLEDevice.h"
-
 #include "sdkconfig.h"
+#include <iostream>
 
-// See the following for generating UUIDs:
-// https://www.uuidgenerator.net/
+using namespace std;
 
-#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
 
 extern "C" {
    void app_main();
 }
 
-static uint8_t  SERVICE_UUID_BIN[] = {0x4f, 0xaf, 0xc2, 0x01, 0x1f, 0xb5, 0x45, 0x9e, 0x8f, 0xcc, 0xc5, 0xc9, 0xc3, 0x31, 0x91, 0x4b};
+
 
 class MyCallbackHandler: public BLECharacteristicCallbacks {
 	void onRead(BLECharacteristic *pCharacteristic) {
+        cout << "*********" << endl;
+        cout << "   READ  " << endl;
+        cout << "*********" << endl;
 		struct timeval tv;
 		gettimeofday(&tv, nullptr);
 		std::ostringstream os;
 		os << "Time: " << tv.tv_sec;
 		pCharacteristic->setValue(os.str());
 	}
+
+    void onWrite(BLECharacteristic *pCharacteristic) {
+    std::string value = pCharacteristic->getValue();
+    if (value.length() > 0) {
+            cout << "*********" << endl;
+            cout << "WRITE " << value << endl;
+            cout << "*********" << endl;
+        }
+    }
 };
 
-static void run() {
-	BLEDevice::init("MYDEVICE");
-	BLEServer *pServer = BLEDevice::createServer();
+static char LOG_TAG[] = "SampleServer";
 
-	BLEService *pService = pServer->createService(BLEUUID(SERVICE_UUID_BIN, 16, true));
+class MainBLEServer: public Task {
+	void run(void *data) {
 
-	BLECharacteristic *pCharacteristic = pService->createCharacteristic(
-		BLEUUID(CHARACTERISTIC_UUID),
-		BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE
-	);
+		BLEDevice::init("SENSAIR");
+		BLEServer* pServer = BLEDevice::createServer();
 
-	pCharacteristic->setCallbacks(new MyCallbackHandler());
+		BLEService* pService = pServer->createService("91bad492-b950-4226-aa2b-4ede9fa42f59");
 
-	pCharacteristic->setValue("Hello World");
+		BLECharacteristic* pCharacteristic = pService->createCharacteristic(
+			BLEUUID("0d563a58-196a-48ce-ace2-dfec78acc814"),
+			BLECharacteristic::PROPERTY_BROADCAST | BLECharacteristic::PROPERTY_READ  |
+			BLECharacteristic::PROPERTY_NOTIFY    | BLECharacteristic::PROPERTY_WRITE |
+			BLECharacteristic::PROPERTY_INDICATE
+		);
 
-	pService->start();
+        
+	    pCharacteristic->setCallbacks(new MyCallbackHandler());
+		pCharacteristic->setValue("Hello World!");
 
-	BLEAdvertising *pAdvertising = pServer->getAdvertising();
-	pAdvertising->start();
-}
+		pService->start();
+
+		BLEAdvertising* pAdvertising = pServer->getAdvertising();
+		pAdvertising->addServiceUUID(BLEUUID(pService->getUUID()));
+		pAdvertising->start();
+		delay(1000000);
+	}
+};
+
+
+void run(void) {
+	MainBLEServer* pMainBleServer = new MainBLEServer();
+	pMainBleServer->setStackSize(20000);
+	pMainBleServer->start();
+
+} 
 
 void app_main(void)
 {
