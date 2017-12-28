7a2a12bf69c52d818dd66c3c47132d76f3b9a04d
Granyy <leogranier@gmail.com>
2017-12-28 18:08:40 +0100

    [ADD] New functionnality (WIP)

--- dev_esp/lib/GasTreatment/GasTreatment.cpp
+++ dev_esp/lib/GasTreatment/GasTreatment.cpp
@@ -23,9 +23,24 @@ void GasTreatment::read_gas() {
 
 void GasTreatment::treat_gas() {
 	read_gas();
-	cout << "Some treatment" << endl;
+	gasRawValue.CO = CO;
+	gasRawValue.CO2 = CO2;
+	gasRawValue.NO2 = NO2;
+	gasRawValue.VOC = VOC;
+	gasValue.CO=map_gas(CO, 0, 80, 0, 100);
+	gasValue.CO2=map_gas(CO2, 400, 500, 0, 100);
+	gasValue.NO2=map_gas(NO2,0.10,0.20,0,100);
+	gasValue.VOC=map_gas(VOC,0,1000,0,100);
+	cout << "CO: " << (int)gasValue.CO << endl;
+	cout << "CO2: " << (int)gasValue.CO2 << endl;
+	cout << "NO2: " << (int)gasValue.NO2 << endl;
+	cout << "VOC: " << (int)gasValue.VOC << endl;
 }
 
 struct gas GasTreatment::get_gasValue() {
 	return gasValue;
+}
+
+struct gasRaw GasTreatment::get_gasRawValue() {
+	return gasRawValue;
 }
--- dev_esp/lib/GasTreatment/GasTreatment.h
+++ dev_esp/lib/GasTreatment/GasTreatment.h
@@ -16,6 +16,11 @@
 #include "GroveGasSensor.h"
 #include "MicsGasSensor.h"
 
+#define map_gas(x, in_min, in_max, out_min, out_max) ((x) - (in_min)) * ((out_max) - (out_min)) / ((in_max) - (in_min)) + (out_min)
+#define GREEN_TH 20
+#define YELLOW_TH 50
+#define RED_TH 80
+
 
 using namespace std;
 
@@ -27,21 +32,30 @@ struct gas {
     uint8_t NO2;
 };
 
+struct gasRaw {
+    float CO;
+    float CO2;
+    float VOC;
+    float NO2;
+};
+
 class GasTreatment {
 	private :
 		GroveGasSensor groveGasSensor;
 		MicsGasSensor micsGasSensor;
-		float CO;
-		float CO2;
-		float VOC;
-		float NO2;
+		float CO = -1;
+		float CO2 = -1;
+		float VOC = -1;
+		float NO2 = -1;
 		struct gas gasValue;
+		struct gasRaw gasRawValue;
 
 	public :
 		void begin();
 		void read_gas();
 		void treat_gas();
 		struct gas get_gasValue();
+		struct gasRaw get_gasRawValue();
 };
 
 
